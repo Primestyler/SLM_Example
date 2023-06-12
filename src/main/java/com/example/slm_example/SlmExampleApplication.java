be3748acc96f70f3f67606602475a597cf483b74
YOUR NAME <YOUR_EMAIL>
2023-06-12 15:02:39 +0200

    first commit

---
+++ src/main/java/com/example/slm_example/SlmExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.slm_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmExampleApplication.class, args);
+    }
+
+}
