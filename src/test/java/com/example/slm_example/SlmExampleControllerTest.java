b478819113c56236c8aeb6b82b67ab5d0ae50758
YOUR NAME <YOUR_EMAIL>
2023-06-13 12:28:37 +0200

    add calcHamming function and the belonging test case

--- src/test/java/com/example/slm_example/SlmExampleControllerTest.java
+++ src/test/java/com/example/slm_example/SlmExampleControllerTest.java
@@ -1,6 +1,8 @@
 package com.example.slm_example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,5 +19,17 @@ class SlmExampleControllerTest {
 
         // Assert
         assertEquals(expected, actual);
+    }
+    @Test
+    void calculateHammingWeightTest() {
+        // Arrange
+        String inputString = "012012";
+
+        // Act
+        int result = s.calculateHammingWeight(inputString);
+
+        // Assert
+        assertEquals(4, result);
+
     }
 }
